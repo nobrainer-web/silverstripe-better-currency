ca9fd5c3614cba77a2d0d1e3b41e16fb479a0dc3
Sander Hagenaars <sander.ha@hotmail.com>
2019-05-16 10:12:41 +0200

    init

---
+++ src/ORM/FieldType/Currency.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanderhagenaars
+ * Date: 2019-05-16
+ * Time: 10:07
+ */
+
+namespace BetterCurrency\ORM\FieldType;
+
+
+class Currency extends \SilverStripe\ORM\FieldType\DBCurrency
+{
+
+}
